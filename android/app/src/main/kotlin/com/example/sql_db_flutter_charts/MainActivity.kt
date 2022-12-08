0c6839486414cadbfadac301759274a875099bcd
Devi Aruna <deviaruna.murugan@syncfusion.com>
2022-12-08 18:04:46 +0530

    FLUT-7109-[bug][flutter]: Done the sample runnable

--- android/app/src/main/kotlin/com/example/sql_db_flutter_charts/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sql_db_flutter_charts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ data_bind_sqlite_cartesian/android/app/src/main/kotlin/com/example/data_bind_sqlite_cartesian/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.data_bind_sqlite_cartesian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
